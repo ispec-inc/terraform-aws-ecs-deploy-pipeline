06ea7fc96f4d617f544ba22f09309de36f35f2a8
murawakimitsuhiro <mitsuhiromurawaki@gmail.com>
2020-03-20 18:46:07 +0900

    Upgrade example/api-server-ssl 0.12

--- examples/api-server-ssl/main.tf
+++ examples/api-server-ssl/main.tf
@@ -2,15 +2,32 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-module "ecs-pipeline" {
-  source  = "../.."
-  version = "0.1.1"
+locals {
+  domain                 = "drone.ispec.tech"
+  application_name       = "simple-go-ping-api"
+  application_name_lower = replace(lower(local.application_name), "/[^a-z0-9]/", "")
+}
 
-  cluster_name        = "example"
-  alb_port            = "8005"
-  container_port      = "8005"
-  app_repository_name = "example-ecr"
-  container_name      = "example-container"
+data "aws_acm_certificate" "example" {
+  domain      = local.domain
+  statuses    = ["ISSUED"]
+  most_recent = true
+}
+
+module "ecs-deploy-pipeline" {
+  source  = "ispec-inc/ecs-deploy-pipeline/aws"
+  version = "0.4.1"
+
+  vpc_id         = "vpc-1e337679"
+  public_subnets = ["subnet-26631d7d", "subnet-41776669"]
+
+  cluster_name        = local.application_name
+  app_repository_name = local.application_name
+  container_name      = local.application_name
+
+  helth_check_path = "/ping"
+  alb_port         = "8005"
+  container_port   = "8005"
 
   git_repository = {
     owner  = "murawakimitsuhiro"
@@ -18,13 +35,6 @@ module "ecs-pipeline" {
     branch = "feature/only-ping"
   }
 
-  helth_check_path = "/ping"
-
-  domain_name         = "${var.domain}"
-  ssl_certificate_arn = "${data.aws_acm_certificate.example.arn}"
-}
-
-data "aws_acm_certificate" "example" {
-  domain   = "${var.domain}"
-  statuses = ["ISSUED", "PENDING_VALIDATION"]
+  domain_name         = local.domain
+  ssl_certificate_arn = data.aws_acm_certificate.example.arn
 }
