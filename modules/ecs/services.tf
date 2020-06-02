451e43dc934b1f4d44f2bd1c571c853fec6ca13c
murawakimitsuhiro <mitsuhiromurawaki@gmail.com>
2020-06-02 12:24:17 +0900

    update delplooyment controller ecs

--- modules/ecs/services.tf
+++ modules/ecs/services.tf
@@ -13,22 +13,14 @@ resource "aws_ecs_service" "web-api" {
   launch_type     = "FARGATE"
   desired_count   = var.desired_tasks
 
-  deployment_controller {
-    type = "CODE_DEPLOY"
-  }
+  //  deployment_controller {
+  //    type = "CODE_DEPLOY"
+  //  }
 
   network_configuration {
-    # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
-    # force an interpolation expression to be interpreted as a list by wrapping it
-    # in an extra set of list brackets. That form was supported for compatibility in
-    # v0.11, but is no longer supported in Terraform v0.12.
-    #
-    # If the expression in the following list itself returns a list, remove the
-    # brackets to avoid interpretation as a list of lists. If the expression
-    # returns a single list item then leave it as-is and remove this TODO comment.
     security_groups  = local.security_group_ids
     subnets          = var.availability_zones
-    assign_public_ip = true
+    assign_public_ip = true // false
   }
 
   load_balancer {
